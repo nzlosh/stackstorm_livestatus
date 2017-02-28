7dd9c5ea67296ba223a15c442c93b1208924a1d5
Carlos <nzlosh@yahoo.com>
2017-02-28 15:31:17 +0100

    Add the detection of empty lists when evaluating results.  To workaround a bug in livestatus returning an empty list when shinken is restarted.

--- actions/get_data.py
+++ actions/get_data.py
@@ -90,15 +90,17 @@ class LiveStatus(object):
     LiveStatus class provides network access to the Live status server.
     """
     def __init__(self, host, port, max_recv=4096,
-                 query_max_retries=3,
+                 query_max_retries=5,
                  query_duration=5,
-                 query_retry_delay=10):
+                 query_retry_delay=60,
+                 allow_empty_list=True):
         self.host = host
         self.port = int(port)
         self.max_recv = int(max_recv)
         self.query_duration = query_duration           # unit=seconds
         self.query_retry_delay = query_retry_delay     # unit=seconds
         self.query_max_retries = query_max_retries
+        self.query_attempt = 0
 
     def execute(self, query):
         """
@@ -107,13 +109,12 @@ class LiveStatus(object):
 
         @query - a livestatus query.
         """
-        answer = None
-        query_attempt = 0
+        result = None
         start = int(time.time())
 
-        while query_attempt < self.query_max_retries:
+        while self.query_attempt < self.query_max_retries:
             try:
-                LOG.debug("Attempt number {}".format(query_attempt))
+                LOG.debug("Attempt number {}".format(self.query_attempt))
                 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server.settimeout(float(self.query_duration))
                 server.connect((self.host, self.port))
@@ -121,47 +122,57 @@ class LiveStatus(object):
                 server.send(query.encode('utf-8').rstrip() + LS_EOL)
                 # Notify server that transmission has finished.
                 server.shutdown(socket.SHUT_WR)
-                answer = ''
-                buf = server.recv(self.max_recv)
-                while buf:
-                    answer += buf
-                    buf = server.recv(self.max_recv)
+                buffer_ = ''
+                data = server.recv(self.max_recv)
+                while data:
+                    buffer_ += data
+                    data = server.recv(self.max_recv)
                     if (int(time.time()) - start > self.query_duration):
                         raise TimeoutException()
+                result = buffer_
+                server.close()
                 break
             except socket.timeout as e:
-                LOG.error("Timeout {}/{}".format(query_attempt, self.query_max_retries))
-                query_attempt += 1
+                LOG.error("Timeout {}/{}".format(self.query_attempt, self.query_max_retries))
+                self.query_attempt += 1
                 time.sleep(self.query_retry_delay)
             except socket.error as e:
                 LOG.error("Socket error occurred! {}".format(type(e)))
-                query_attempt += 1
+                self.query_attempt += 1
                 time.sleep(self.query_retry_delay)
             except TimeoutException as e:
                 LOG.error("Livestatus didn't respond within the time allocated.")
-                query_attempt += 1
-                server.close()
+                self.query_attempt += 1
                 time.sleep(self.query_retry_delay)
             except Exception as e:
                 LOG.error("Unhandled error occurred: {}".format(sys.exc_info()))
                 break
-        return answer
+        return result
 
-    def get_json(self, query):
+    def get_json(self, query, allow_empty_list=True):
         """
         Parse the result of a live status query as JSON.
 
         @query - a livestatus query. The query MUST request the result
                  is returned in JSON format.
         """
-        res = None
-        try:
-            res = json.loads(self.execute(query))
-        except TypeError as e:
-            LOG.error("No data received from livestatus API")
-        except ValueError as e:
-            LOG.error("Error while deserialising JSON from livestatus API.")
-        return res
+        result = None
+        while self.query_attempt < self.query_max_retries:
+            try:
+                data = json.loads(self.execute(query))
+                if allow_empty_list is not True and data == []:
+                    raise ValueError()
+                result = data
+                break
+            except TypeError as e:
+                LOG.error("No or invalid data received from livestatus API")
+                self.query_attempt += 1
+                time.sleep(self.query_retry_delay)
+            except ValueError as e:
+                LOG.error("Error while deserialising JSON from livestatus API.")
+                self.query_attempt += 1
+                time.sleep(self.query_retry_delay)
+        return result
 
 
 class Get(Action):
@@ -170,7 +181,8 @@ class Get(Action):
     """
     def run(self, table='', columns=None, filters=None,
             stats=None, limit=None, output_format="json",
-            query_max_retries=None, query_duration=None, query_retry_delay=None):
+            query_max_retries=None, query_duration=None,
+            query_retry_delay=None, allow_empty_list=True):
         """
         The run method to be called by Stackstorm.
 
@@ -184,38 +196,35 @@ class Get(Action):
         host = self.config['host']
         port = self.config['port']
 
-        extra_kwargs = {}
+        passthrough = {}
         if query_max_retries is not None:
-            extra_kwargs["query_max_retries"] = query_max_retries
+            passthrough["query_max_retries"] = query_max_retries
         if query_duration is not None:
-            extra_kwargs["query_duration"] = query_duration
+            passthrough["query_duration"] = query_duration
         if query_retry_delay is not None:
-            extra_kwargs["query_retry_delay"] = query_retry_delay
+            passthrough["query_retry_delay"] = query_retry_delay
 
         result = (False, "An error occurred fetching data from Livestatus.")
 
-        live_status = LiveStatus(host, port, **extra_kwargs)
-        if live_status is None:
-            result = (False, 'Error connecting to livestatus.')
-
+        live_status = LiveStatus(host, port, **passthrough)
         query = 'GET {}{}'.format(table, LS_EOL)
 
-        if columns:
+        if columns is not None:
             query += self._process_columns(columns)
 
-        if filters:
+        if filters is not None:
             query += self._process_filters(filters)
 
-        if stats:
+        if stats is not None:
             query += self._process_stats(stats)
 
-        if limit:
+        if limit is not None:
             query += 'Limit: {}{}'.format(limit, LS_EOL)
 
         tmp = None
         if output_format.lower() == 'json':
             query += 'OutputFormat: json{}'.format(LS_EOL)
-            tmp = live_status.get_json(query)
+            tmp = live_status.get_json(query, allow_empty_list)
         else:
             tmp = live_status.execute(query)
 
